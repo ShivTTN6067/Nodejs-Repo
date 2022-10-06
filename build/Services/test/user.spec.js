fcff7d1c64a77272bf4eb21cf71f847368c6f44c
shivang <shivanggarg48@gmail.com>
2022-10-07 00:38:10 +0530

    jest and code standards are add

--- build/Services/test/user.spec.js
+++ build/Services/test/user.spec.js
@@ -1,27 +1,32 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// Requiring module
 const globals_1 = require("@jest/globals");
 const user_1 = require("../user");
-// We can group similar tests inside a describe block
-(0, globals_1.describe)("Simple Calculations", () => {
-    // We can add nested blocks for different tests
+(0, globals_1.describe)("Test cases for user CRUD operations", () => {
     (0, globals_1.describe)("Test1", () => {
-        it("Test user getNameService", () => {
+        it("Test user getUserData", () => {
             (0, globals_1.expect)((0, user_1.getUserData)(0)).toStrictEqual({ name: "John" });
         });
         it("Test user name record is not exist ", () => {
             (0, globals_1.expect)((0, user_1.getUserData)(1)).toBe(undefined);
         });
+        it(" get all users data ", () => {
+            (0, globals_1.expect)((0, user_1.getAllUsersData)()).toStrictEqual([{ name: "John" }]);
+        });
+        it("Test user create name record", () => {
+            (0, globals_1.expect)((0, user_1.createUserData)({ name: "abc", class: 12 })).toBe("created");
+        });
         it("Test user delete name record", () => {
             (0, globals_1.expect)((0, user_1.deleteUserData)(0)).toStrictEqual({ name: "John" });
         });
         it("Test user delete name record", () => {
-            // expect(updateUserData(0)).toBe({
-            // 	name: "fgdxgfhj",
-            // 	role: 1,
-            // 	ee: "DEe"
-            // });
+            (0, globals_1.expect)((0, user_1.updateUserData)(0, {
+                "name": "fgdxgfhj",
+                "class": 1
+            })).toStrictEqual({
+                "name": "fgdxgfhj",
+                "class": 1
+            });
         });
     });
 });
--- src/Services/test/user.spec.ts
+++ src/Services/test/user.spec.ts
@@ -1,29 +1,41 @@
-// Requiring module
-import {describe, expect} from "@jest/globals";
+import { describe, expect } from "@jest/globals";
 import { getAllUsersData, getUserData, createUserData, deleteUserData, updateUserData } from "../user";
-// We can group similar tests inside a describe block
-describe("Simple Calculations", () => {
 
-	// We can add nested blocks for different tests
+describe("Test cases for user CRUD operations", () => {
+
 	describe("Test1", () => {
-		it("Test user getNameService", () => {
-			expect(getUserData(0)).toStrictEqual({name:"John"});
+		it("Test user getUserData", () => {
+			expect(getUserData(0)).toStrictEqual({ name: "John" });
 		});
 
 		it("Test user name record is not exist ", () => {
 			expect(getUserData(1)).toBe(undefined);
 		});
 
-		it("Test user delete name record", () => {
-			expect(deleteUserData(0)).toStrictEqual({name:"John"});
+		it(" get all users data ", () => {
+			expect(getAllUsersData()).toStrictEqual([{ name: "John" }]);
+		});
+
+		it("Test user create name record", () => {
+			expect(createUserData({name:"abc", class:12})).toBe("created");
 		});
 
 		it("Test user delete name record", () => {
-			// expect(updateUserData(0)).toBe({
-			// 	name: "fgdxgfhj",
-			// 	role: 1,
-			// 	ee: "DEe"
-			// });
+			expect(deleteUserData(0)).toStrictEqual({ name: "John" });
+		});
+
+		it("Test user delete name record for non exist", () => {
+			expect(deleteUserData(2)).toBe("");
+		});
+
+		it("Test user delete name record", () => {
+			expect(updateUserData(0, {
+				"name": "fgdxgfhj",
+				"class": 1
+			})).toStrictEqual({
+				"name": "fgdxgfhj",
+				"class": 1
+			});
 		});
 
 	});

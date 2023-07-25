0c3116c0a7e6093acd84a5bdf57f5f5fe9f22df3
Shivang Garg <shivanggarg@Shivangs-MacBook-Pro.local>
2023-07-25 18:45:31 +0530

    Error handling

--- src/authentication/controller/auth-controller.ts
+++ src/authentication/controller/auth-controller.ts
@@ -3,31 +3,31 @@ import auth from '../service/auth-service';
 
 class AuthController {
 
-  authentication = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      const authToken = await auth.authentication(req.body);
-      res.status(200).send({ authToken: authToken });
-    } catch (err) {
-      console.error(err);
-      res.statusCode = 401;
-      res.json({
-        error: err
-      });
-    }
-  };
+	authentication = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+		try {
+			const authToken = await auth.authentication(req.body);
+			res.status(200).send({ authToken: authToken });
+		} catch (err) {
+			console.error(err);
+			res.statusCode = 401;
+			res.json({
+				error: err
+			});
+		}
+	};
 
-  signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      await auth.registerUser(req.body);
-      res.status(200).send({ status: 'success', signUp: true });
-    } catch (err) {
-      console.error(err);
-      res.statusCode = 401;
-      res.json({
-        error: err
-      });
-    }
-  };
+	signUp = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+		try {
+			await auth.registerUser(req.body);
+			res.status(200).send({ status: 'success', signUp: true });
+		} catch (err) {
+			console.error(err);
+			res.statusCode = 401;
+			res.json({
+				error: err
+			});
+		}
+	};
 
 }
 
